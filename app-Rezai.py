21e70073ace03ad0a76a5bf7896172e8bce17da8
Matin Rezai <rezaimatin72@gmail.com>
2025-05-05 11:56:28 +0200

    test 2

--- app-Rezai.py
+++ app-Rezai.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Helle, ALED WTF DEVOPS NANI,test video"
+    return "Helle, ALED WTF DEVOPS NANI,test video,1,1"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
