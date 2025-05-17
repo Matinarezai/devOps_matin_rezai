927e47be64f853c7a3210340c563a1d82ba99672
Matin Rezai <rezaimatin72@gmail.com>
2025-05-17 08:15:02 +0200

    modification propre pour la video clean

--- app-Rezai.py
+++ app-Rezai.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Helle, ALED WTF DEVOPS NANI,test video pour exercice test"
+    return "Helle, ALED WTF DEVOPS NANI, je vais faire mon derner modif "
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
