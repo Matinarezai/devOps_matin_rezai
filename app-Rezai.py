77a5c36068c55845116d2fc1c18dd3beb3a14d71
Zakir1373 <zakir.haidari@exoscale.com>
2025-05-17 11:47:25 +0000

    change

--- app-Rezai.py
+++ app-Rezai.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Bienvunue au Flask, ce site est pour tester zakir"
+    return "Bienvunue au Flask, ce site est pour tester zakir haidari"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
